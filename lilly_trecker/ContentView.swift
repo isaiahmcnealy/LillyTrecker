d73b657da323fd81f34643eb5ca18b0af43db933
Isaiah McNealy <53882361+isaiahmcnealy@users.noreply.github.com>
2022-11-09 05:37:00 -0500

    Add files via upload

---
+++ lilly_trecker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  lilly_trecker
+//
+//  Created by Isaiah McNealy on 11/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lilly_trecker/lilly_treckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  lilly_treckerApp.swift
+//  lilly_trecker
+//
+//  Created by Isaiah McNealy on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct lilly_treckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
